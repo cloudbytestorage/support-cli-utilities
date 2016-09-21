94cae217d893272dd0bc6617a673983297587b78
Ranjith R <ranjithr005@gmail.com>
2016-09-21 22:04:17 +0530

    Modified
    Segregated protocol details, Added a folder for detailed view. Will get to know whether jail ip is binded or not.
    Simplified way to understand the services running under each protocol

--- vsm_health_check.sh
+++ vsm_health_check.sh
@@ -1,32 +1,53 @@
 #!/bin/sh
+## This script gets detail about VSM health
 #usage= {sh vsm_health_check.sh <vsmip>}
+timestamp=$(date +"%d-%m-%Y")
+FILE=/root/"VSM_Health_$timestamp"
+mkdir -p $FILE
+cd $FILE
+echo "Detailed outuput in /root/"VSM_Health_$timestamp
 RED='\033[0;31m'
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+BLACK='\033[0;30m'
 NC='\033[0m'
 jip="$1"
 echo -e "${RED}Jail IP:${NC}$jip\n"
+
+jlsipbind=`/sbin/ifconfig |grep -c "$jip"`
+/sbin/ifconfig | grep -B 3 "$jip" > jlsifconfig.out
+if [ $jlsipbind -eq 0 ]
+then
+ echo -e "$jip is not binded\n"
+else
+ echo -e "$jip is binded\n"
+fi
+
 jlsid="$(jls | grep "$jip"|awk '{printf "%s", $1}')"
+jlspath="$(jls | grep "$jip"|awk '{printf "%s", $4}')"
 echo -e "${RED}Jail Id:${NC}$jlsid\n"
-echo -e "${RED}Services running under this VSM are:${NC}\n"
-jlsmountd="$(jexec "$jlsid" service mountd onestatus)"
-echo -e "$jlsmountd\n"
-jlsnfsd="$(jexec "$jlsid" service nfsd onestatus)"
-echo -e "$jlsnfsd\n"
-jlsstad="$(jexec "$jlsid" service statd onestatus)"
-echo -e "$jlsstad\n"
-jlslockd="$(jexec "$jlsid" service lockd onestatus)"
-echo -e "$jlslockd\n"
-jlsrpcbind="$(jexec "$jlsid" service rpcbind onestatus)"
-echo -e "$jlsrpcbind\n"
-echo -e "${RED}Export File:${NC}\n"
-jlsexport="$(jexec  $jlsid cat /etc/export)"
-echo -e "$jlsexport\n"
+echo -e "${RED}Jail path:${NC}$jlspath\n"
 echo -e "${RED}zfs list of this  VSM are:${NC}\n"
 jexeccmd="$(jexec $jlsid zfs list|grep -v tpool)"
 echo -e "$jexeccmd\n"
-echo -e "${RED}istgt details${NC}\n"
-jlsistgt="$(jexec $jlsid istgtcontrol dump)"
-echo -e "$jlsistgt\n"
-echo -e "${RED}Clients connected to this VSM are :${NC}\n"
-jlsnetstat="$(jexec "$jlsid" netstat -an | grep -i est | grep "3260\|2049\|445")"
-echo "$jlsnetstat"
 
+echo -e "${BLUE}NFS Details${NC}\n"
+jlsexport="$(jexec  $jlsid cat /etc/export)" 
+echo "$jlsexport" > export.out  2>&1
+echo -e "${CYAN}Services running status:${NC}\n"
+jlslockd="$(jexec $jlsid service lockd onestatus)"
+jlsmountd="$(jexec $jlsid service mountd onestatus)"
+jlsstatd="$(jexec $jlsid service statd  onestatus)"
+jlsrpcbind="$(jexec $jlsid service rpcbind onestatus)"
+jlsnfsd="$(jexec $jlsid service nfsd onestatus)"
+echo -e "$jlslockd\n$jlsmountd\n$jlsstatd\n$jlsrpcbind\n$jlsnfsd\n"
+
+echo -e "${BLUE}iSCSI details${NC}\n"
+echo -e "${CYAN}Services running status:${NC}\n"
+jlsiscsi="$(jexec $jlsid service istgt onestatus)"
+echo -e "$jlsiscsi\n"
+
+jlsistgt="$(jexec $jlsid istgtcontrol dump)"
+echo  "$jlsistgt" > istgtcontrol_dump.out
+jlsnetstat="$(jexec "$jlsid" netstat -an | grep -i est | grep "3260\|2049\|445")"
+echo "$jlsnetstat" > clientconnected.out
